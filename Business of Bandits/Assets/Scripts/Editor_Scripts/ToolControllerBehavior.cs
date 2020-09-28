f8db99e5765d502b85273cd4dc0372fa620b72e1
DanyFids <danyfids@gmail.com>
2020-09-28 16:30:08 -0400

    Branch init
    now with transform widget!

---
+++ Business of Bandits/Assets/Scripts/Editor_Scripts/ToolControllerBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolControllerBehavior : MonoBehaviour
+{
+	//Widget drop off
+	public GameObject Transform_Widget;
+
+	//Widget Axes
+	private GameObject Translate_X_Axis;
+	private GameObject Translate_Y_Axis;
+	private GameObject Translate_Z_Axis;
+
+	private GameObject selectedObj = null;
+
+	private void Start()
+	{
+		if (Transform_Widget != null)
+		{
+			Translate_X_Axis = Transform_Widget.transform.Find("x-axis").gameObject;
+			Translate_Y_Axis = Transform_Widget.transform.Find("y-axis").gameObject;
+			Translate_Z_Axis = Transform_Widget.transform.Find("z-axis").gameObject;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+}
