78cf87307af8ea431278a6e7e7179841f08dc676
DanyFids <danyfids@gmail.com>
2020-10-04 14:02:17 -0400

    some small changes

---
+++ Business of Bandits/Assets/Scripts/Editor_Scripts/Object_Button_Script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Button_Script : MonoBehaviour
+{
+	public GameObject Template_Object;
+	public Camera Main_Camera;
+
+	private GameObject new_obj = null;
+
+	private void OnMouseDown()
+	{
+		if (new_obj == null)
+		{
+			new_obj = Instantiate(Template_Object);
+			new_obj.tag = "place_obj";
+		}
+	}
+
+	private void OnMouseDrag()
+	{
+		if (new_obj != null) {
+			RaycastHit hit;
+			Ray ray = Main_Camera.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit, 10.0f)) {
+				new_obj.transform.position = hit.point;
+			}
+		}
+	}
+
+	private void OnMouseUp()
+	{
+		new_obj.tag = "editor_obj";
+		new_obj = null;
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
