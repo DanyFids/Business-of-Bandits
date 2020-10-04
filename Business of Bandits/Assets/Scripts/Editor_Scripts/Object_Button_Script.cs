c5211079d68bfb0b8bb94085f7f58af46e98bba6
DanyFids <danyfids@gmail.com>
2020-10-04 19:43:38 -0400

    finished?

--- Business of Bandits/Assets/Scripts/Editor_Scripts/Object_Button_Script.cs
+++ Business of Bandits/Assets/Scripts/Editor_Scripts/Object_Button_Script.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class Object_Button_Script : MonoBehaviour
 {
-	public GameObject Template_Object;
+	//private static int EDITOR_TOOL_LAYER = 8;
+	//private static int EDITOR_SUPPORT_LAYER = 9;
+	private static int EDITOR_OBJECT_LAYER = 10;
+	private static int PLACE_OBJECT_LAYER = 11;
+
+	public GameObject Template_Object = null;
 	public Camera Main_Camera;
+	public ToolControllerBehavior Tool_Controller;
 
 	private GameObject new_obj = null;
+	private float place_distance = 10.0f;
 
 	private void OnMouseDown()
 	{
-		if (new_obj == null)
+		if (new_obj == null && Template_Object != null)
 		{
 			new_obj = Instantiate(Template_Object);
 			new_obj.tag = "place_obj";
+			new_obj.layer = PLACE_OBJECT_LAYER;
+			Rigidbody r;
+			if (r = new_obj.GetComponent<Rigidbody>())
+			{
+				r.useGravity = false;
+				r.detectCollisions = false;
+			}
+
+			if (Tool_Controller != null)
+				Tool_Controller.ToggleDisabled(true);
 		}
 	}
 
@@ -23,9 +40,17 @@ public class Object_Button_Script : MonoBehaviour
 		if (new_obj != null) {
 			RaycastHit hit;
 			Ray ray = Main_Camera.ScreenPointToRay(Input.mousePosition);
+			LayerMask layerMask = ~(LayerMask.GetMask("Place Objects", "UI"));
 
-			if (Physics.Raycast(ray, out hit, 10.0f)) {
+			place_distance += Input.mouseScrollDelta.y;
+
+			if (Physics.Raycast(ray, out hit, place_distance, layerMask))
+			{
 				new_obj.transform.position = hit.point;
+			}
+			else
+			{
+				new_obj.transform.position = ray.origin + (ray.direction * place_distance);
 			}
 		}
 	}
@@ -33,7 +58,17 @@ public class Object_Button_Script : MonoBehaviour
 	private void OnMouseUp()
 	{
 		new_obj.tag = "editor_obj";
+		new_obj.layer = EDITOR_OBJECT_LAYER;
+		Rigidbody r;
+		if (r = new_obj.GetComponent<Rigidbody>())
+		{
+			r.useGravity = true;
+			r.detectCollisions = true;
+		}
 		new_obj = null;
+
+		if(Tool_Controller != null)
+			Tool_Controller.ToggleDisabled(false);
 	}
 
 	// Start is called before the first frame update
